c6093fff1a6afc5b20c62632b20df33599290eb4
David Earn <earn@math.mcmaster.ca>
2022-08-02 09:46:06 -0400

    install the version of epigrowthfit used for the PNAS plague growth paper

--- install_pkgs.R
+++ install_pkgs.R
@@ -6,8 +6,13 @@ pkgs <- pkgs[!pkgs %in% rownames(i1)]
 install.packages(pkgs)
 library(remotes)
 install_github("bbolker/bbmle")
-install_github("davidearn/epigrowthfit",dependencies=TRUE)
-
-
-
-
+##
+## The current version of epigrowthfit is installed via
+##     install_github("davidearn/epigrowthfit",dependencies=TRUE)
+## but this current version is not backwards-compatible and will not
+## work with the code for the PNAS plague growth paper.  Therefore, we
+## install the version that was released with the PNAS paper:
+install_github("davidearn/epigrowthfit",
+               ref = "pnas",
+               dependencies = TRUE,
+               build_vignettes = TRUE)			

a004e4d9cd0ab1d8d4d126c7cf6eaa1f08706491
Ben Bolker <bbolker@gmail.com>
2022-08-04 12:08:27 -0400

    package install machinery updates

--- install_pkgs.R
+++ install_pkgs.R
@@ -4,19 +4,15 @@
 options(repos=c(CRAN="https://cloud.r-project.org"))
 pkgs <- scan("required_packages", comment="#", what=character(0), quiet=TRUE)
 i1 <- installed.packages()
-pkgs <- pkgs[!pkgs %in% rownames(i1)]
+pkgs <- setdiff(pkgs, rownames(i1))
 install.packages(pkgs)
-
-## install the correct version of bbmle??
-library(remotes)
-install_github("bbolker/bbmle")
 
 ## Install an older version of epigrowthfit (provides the package epigrowthfitPNAS)
 ##     install_github("davidearn/epigrowthfit",dependencies=TRUE)
 ## but this current version is not backwards-compatible and will not
 ## work with the code for the PNAS plague growth paper.  Therefore, we
 ## install the version that was released with the PNAS paper:
-install_github("davidearn/epigrowthfit",
+remotes::install_github("davidearn/epigrowthfit",
                ref = "pnas",
                dependencies = TRUE,
                build_vignettes = TRUE)			
--- library.R
+++ library.R
@@ -1,6 +1,8 @@
 p <- scan("required_packages", comment="#", what=character(0), quiet=TRUE)
-p <- c(p, "epigrowthfit", "bbmle")
-res <- suppressMessages(vapply(p,require,character.only=TRUE,logical(1)))
+p <- c(p, "epigrowthfitPNAS")
+res <- suppressPackageStartupMessages(
+    vapply(p, require, quietly = TRUE, character.only=TRUE, logical(1))
+    )
 if (any(!res)) {
     missing <- p[!res]
     stop("please install these missing packages: ",
